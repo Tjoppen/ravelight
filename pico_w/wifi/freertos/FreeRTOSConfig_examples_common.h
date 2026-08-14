3907a169f87173208859cd32b09064bdb4e73d08
Tomas Härdin <git@haerdin.se>
2026-08-14 14:29:19 +0200

    Switch to 48 kHz timeslice

--- pico_w/wifi/freertos/FreeRTOSConfig_examples_common.h
+++ pico_w/wifi/freertos/FreeRTOSConfig_examples_common.h
@@ -45,7 +45,8 @@
 #define configUSE_TICKLESS_IDLE                 0
 #define configUSE_IDLE_HOOK                     0
 #define configUSE_TICK_HOOK                     0
-#define configTICK_RATE_HZ                      ( ( TickType_t ) 1000 )
+// 96 kHz won't work because quiet mode calls sin() which takes too many cycles
+#define configTICK_RATE_HZ                      ( ( TickType_t ) 48000 )
 #define configMAX_PRIORITIES                    32
 #define configMINIMAL_STACK_SIZE                ( configSTACK_DEPTH_TYPE ) 512
 #define configUSE_16_BIT_TICKS                  0
